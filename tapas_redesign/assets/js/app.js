0a40a8b0025761bee9a7bbc2b0c1f42a24fc05cc
Eli Zoller <zoller.eli@gmail.com>
2015-04-15 14:33:21 -0400

    working styling into a drupal subtheme of bootstrap

--- tapas_redesign/assets/js/app.js
+++ tapas_redesign/assets/js/app.js
@@ -1,38 +1,39 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+(function ($) {
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+}(jQuery));
+
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
+jQuery('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+jQuery('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
 //trigger carousel
-$(document).ready(function() {
-    $('#myCarousel').carousel({
+jQuery(document).ready(function() {
+  jQuery('#myCarousel').carousel({
 	    interval: 10000
 	})
 });
 
 //collapses clicks on outside click for collapses
-$(document).on('click',function(){
-	$('.collapse').collapse('hide');
+jQuery(document).on('click',function(){
+  jQuery('.collapse').collapse('hide');
 })
 
 //toggle star to favorite
-$(".record-helpers a.favorite").click(function(e){
+jQuery(".record-helpers a.favorite").click(function(e){
   e.preventDefault();
-  $(this).find("i").toggleClass("fa-bookmark-o").toggleClass("fa-bookmark");
+  jQuery(this).find("i").toggleClass("fa-bookmark-o").toggleClass("fa-bookmark");
 })
