d49e77d702597f7eb222f0d43b25aa4e32d419c0
Eli Zoller <zoller.eli@gmail.com>
2015-06-02 11:06:02 -0400

    Removing cruft and adding drupal bootstrap theme functions to handle the settings page

--- tapas_redesign/assets/js/app.js
+++ tapas_redesign/assets/js/app.js
@@ -1,15 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-(function ($) {
-  $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-  });
-}(jQuery));
-
-
 // Highlight the top nav as scrolling occurs
 jQuery('body').scrollspy({
     target: '.navbar-fixed-top'
@@ -20,22 +8,194 @@ jQuery('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-//trigger carousel
-jQuery(document).ready(function() {
-  jQuery('#myCarousel').carousel({
-	    interval: 10000
-	})
-  //trying to put all the tooltips on top instead of right because it pushes them off the screen
-  jQuery('[data-toggle="tooltip"]').data('bs.tooltip').options.placement = 'top';
-});
-
-// //collapses clicks on outside click for collapses - taking this out because it was causing collapses to close automatically even if you click inside the collapse itself
-// jQuery(document).on('click',function(){
-//   jQuery('.collapse').collapse('hide');
-// })
+(function($) {
+  $(document).ready(function() {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    //trigger carousel
+    jQuery('#myCarousel').carousel({
+        interval: 10000
+    });
+  });
+})(jQuery);
 
 //toggle star to favorite
 jQuery(".record-helpers a.favorite").click(function(e){
   e.preventDefault();
   jQuery(this).find("i").toggleClass("fa-bookmark-o").toggleClass("fa-bookmark");
 })
+
+
+
+/**
+ * @file
+ * bootstrap.js
+ *
+ * Provides general enhancements and fixes to Bootstrap's JS files.
+ */
+
+var Drupal = Drupal || {};
+
+(function($, Drupal){
+  "use strict";
+
+  Drupal.behaviors.bootstrap = {
+    attach: function(context) {
+      // Provide some Bootstrap tab/Drupal integration.
+      $(context).find('.tabbable').once('bootstrap-tabs', function () {
+        var $wrapper = $(this);
+        var $tabs = $wrapper.find('.nav-tabs');
+        var $content = $wrapper.find('.tab-content');
+        var borderRadius = parseInt($content.css('borderBottomRightRadius'), 10);
+        var bootstrapTabResize = function() {
+          if ($wrapper.hasClass('tabs-left') || $wrapper.hasClass('tabs-right')) {
+            $content.css('min-height', $tabs.outerHeight());
+          }
+        };
+        // Add min-height on content for left and right tabs.
+        bootstrapTabResize();
+        // Detect tab switch.
+        if ($wrapper.hasClass('tabs-left') || $wrapper.hasClass('tabs-right')) {
+          $tabs.on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+            bootstrapTabResize();
+            if ($wrapper.hasClass('tabs-left')) {
+              if ($(e.target).parent().is(':first-child')) {
+                $content.css('borderTopLeftRadius', '0');
+              }
+              else {
+                $content.css('borderTopLeftRadius', borderRadius + 'px');
+              }
+            }
+            else {
+              if ($(e.target).parent().is(':first-child')) {
+                $content.css('borderTopRightRadius', '0');
+              }
+              else {
+                $content.css('borderTopRightRadius', borderRadius + 'px');
+              }
+            }
+          });
+        }
+      });
+    }
+  };
+
+  /**
+   * Bootstrap Popovers.
+   */
+  Drupal.behaviors.bootstrapPopovers = {
+    attach: function (context, settings) {
+      if (settings.bootstrap && settings.bootstrap.popoverEnabled) {
+        var elements = $(context).find('[data-toggle="popover"]').toArray();
+        for (var i = 0; i < elements.length; i++) {
+          var $element = $(elements[i]);
+          var options = $.extend(true, {}, settings.bootstrap.popoverOptions, $element.data());
+          $element.popover(options);
+        }
+      }
+    }
+  };
+
+  /**
+   * Bootstrap Tooltips.
+   */
+  Drupal.behaviors.bootstrapTooltips = {
+    attach: function (context, settings) {
+      if (settings.bootstrap && settings.bootstrap.tooltipEnabled) {
+        var elements = $(context).find('[data-toggle="tooltip"]').toArray();
+        for (var i = 0; i < elements.length; i++) {
+          var $element = $(elements[i]);
+          var options = $.extend(true, {}, settings.bootstrap.tooltipOptions, $element.data());
+          $element.tooltip(options);
+        }
+      }
+    }
+  };
+
+  /**
+   * Anchor fixes.
+   */
+  var $scrollableElement = $();
+  Drupal.behaviors.bootstrapAnchors = {
+    attach: function(context, settings) {
+      var i, elements = ['html', 'body'];
+      if (!$scrollableElement.length) {
+        for (i = 0; i < elements.length; i++) {
+          var $element = $(elements[i]);
+          if ($element.scrollTop() > 0) {
+            $scrollableElement = $element;
+            break;
+          }
+          else {
+            $element.scrollTop(1);
+            if ($element.scrollTop() > 0) {
+              $element.scrollTop(0);
+              $scrollableElement = $element;
+              break;
+            }
+          }
+        }
+      }
+      if (!settings.bootstrap || !settings.bootstrap.anchorsFix) {
+        return;
+      }
+      var anchors = $(context).find('a').toArray();
+      for (i = 0; i < anchors.length; i++) {
+        if (!anchors[i].scrollTo) {
+          this.bootstrapAnchor(anchors[i]);
+        }
+      }
+      $scrollableElement.once('bootstrap-anchors', function () {
+        $scrollableElement.on('click.bootstrap-anchors', 'a[href*="#"]:not([data-toggle],[data-target])', function(e) {
+          this.scrollTo(e);
+        });
+      });
+    },
+    bootstrapAnchor: function (element) {
+      element.validAnchor = element.nodeName === 'A' && (location.hostname === element.hostname || !element.hostname) && element.hash.replace(/#/,'').length;
+      element.scrollTo = function(event) {
+        var attr = 'id';
+        var $target = $(element.hash);
+        if (!$target.length) {
+          attr = 'name';
+          $target = $('[name="' + element.hash.replace('#', '') + '"');
+        }
+        var offset = $target.offset().top - parseInt($scrollableElement.css('paddingTop'), 10) - parseInt($scrollableElement.css('marginTop'), 10);
+        if (this.validAnchor && $target.length && offset > 0) {
+          if (event) {
+            event.preventDefault();
+          }
+          var $fakeAnchor = $('<div/>')
+            .addClass('element-invisible')
+            .attr(attr, $target.attr(attr))
+            .css({
+              position: 'absolute',
+              top: offset + 'px',
+              zIndex: -1000
+            })
+            .appendTo(document);
+          $target.removeAttr(attr);
+          var complete = function () {
+            location.hash = element.hash;
+            $fakeAnchor.remove();
+            $target.attr(attr, element.hash.replace('#', ''));
+          };
+          if (Drupal.settings.bootstrap.anchorsSmoothScrolling) {
+            $scrollableElement.animate({ scrollTop: offset, avoidTransforms: true }, 400, complete);
+          }
+          else {
+            $scrollableElement.scrollTop(offset);
+            complete();
+          }
+        }
+      };
+    }
+  };
+
+})(jQuery, Drupal);
