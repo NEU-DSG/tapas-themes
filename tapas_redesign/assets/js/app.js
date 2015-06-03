7372ee80c85e3241fb7420c30e319e76ad7f92cd
Eli Zoller <zoller.eli@gmail.com>
2015-06-03 10:39:45 -0400

    a few minor tweaks to homebox css and bringing in homebox function to override tipsy with tooltip - i'm not sure this is the best way to do this but it's the only way i can get it to work right now because leaving it using tipsy breaks the buttons

--- tapas_redesign/assets/js/app.js
+++ tapas_redesign/assets/js/app.js
@@ -30,3 +30,141 @@ jQuery(".record-helpers a.favorite").click(function(e){
   e.preventDefault();
   jQuery(this).find("i").toggleClass("fa-bookmark-o").toggleClass("fa-bookmark");
 })
+
+
+
+
+/*this overrides the js from homebox - all i'm doing is replacing .tipsy with .tooltip but i can't think of a better way to do this*/
+(function($) {
+  Drupal.behaviors.homeboxPortlet = {
+  attach: function (context) {
+    $('.homebox-portlet:not(.homebox-processed)', context).addClass('homebox-processed').each(function () {
+      var $portlet = $(this),
+        $portletHeader = $portlet.find('.portlet-header'),
+        $portletSettings = $portlet.find('.portlet-settings'),
+        $portletConfig = $portlet.find('.portlet-config');
+
+      // Restore classes saved before AHAH, they back some page-wide
+      // settings.
+      if (Drupal.homebox.config[$portlet.attr('id')] !== undefined) {
+        $portlet.attr('class', Drupal.homebox.config[$portlet.attr('id')]);
+      }
+
+      // Prevent double-clicks from causing a selection
+      $portletHeader.disableSelection();
+
+      // Attach click event to maximize icon
+      $portletHeader.find('.portlet-maximize').click(function () {
+        $(this).toggleClass('portlet-maximize').toggleClass('portlet-minimize');
+        Drupal.homebox.maximizeBox(this);
+        Drupal.homebox.equalizeColumnsHeights();
+      });
+
+      // Attach click event on minus
+      $portletHeader.find('.portlet-minus').click(function () {
+        $(this).toggleClass('portlet-minus').toggleClass('portlet-plus');
+        $portlet.find('.portlet-content').toggle();
+        Drupal.homebox.equalizeColumnsHeights();
+        Drupal.homebox.pageChanged();
+      })
+      .each(function () {
+        if (!$portlet.find('.portlet-content').is(':visible')) {
+          $(this).toggleClass('portlet-minus').toggleClass('portlet-plus');
+          Drupal.homebox.equalizeColumnsHeights();
+        }
+      });
+
+      // Attach double click event on portlet header
+      $portlet.find('.portlet-title').dblclick(function () {
+        if ($portlet.find('.portlet-content').is(':visible')) {
+          $portletHeader.find('.portlet-minus').toggleClass('portlet-plus').toggleClass('portlet-minus');
+        }
+        else {
+          $portletHeader.find('.portlet-plus').toggleClass('portlet-minus').toggleClass('portlet-plus');
+        }
+        $portlet.find('.portlet-content').toggle();
+
+        Drupal.homebox.equalizeColumnsHeights();
+        Drupal.homebox.pageChanged();
+      });
+
+      // Attach click event on settings icon
+      $portletSettings.click(function () {
+        $portletConfig.toggle();
+        Drupal.homebox.equalizeColumnsHeights();
+      });
+      // Show settings if there are error messages
+      if ($portletConfig.find('>.messages').length) {
+        $portletSettings.trigger('click');
+      }
+      // Save classes on submit
+      $portletConfig.find('.form-submit').click(function () {
+        Drupal.homebox.config[$portlet.attr('id')] = $portlet.attr('class');
+      });
+
+      // Attach click event on close
+      $portletHeader.find('.portlet-close').click(function () {
+        $portlet.hide();
+        Drupal.homebox.equalizeColumnsHeights();
+        Drupal.homebox.pageChanged();
+      });
+
+      $.each($portlet.attr('class').split(' '), function (key, a) {
+        if (a.substr(0, 14) === 'homebox-color-') {
+          $portletHeader.attr('style', 'background: #' + a.substr(14));
+          $portlet.find('.homebox-portlet-inner').attr('style', 'border: 1px solid #' + a.substr(14));
+        }
+      });
+
+      // Add click behaviour to color buttons
+      $portlet.find('.homebox-color-selector').click(function () {
+        var color = $(this).css('background-color');
+        color = Drupal.homebox.hexColor(color);
+
+        $.each($portlet.attr('class').split(' '), function (key, value) {
+          if (value.indexOf('homebox-color-') === 0) {
+            $portlet.removeClass(value);
+          }
+        });
+
+        // Add color classes to blocks
+        // This is used when we save so we know what color it is
+        $portlet.addClass('homebox-color-' + color.replace('#', ''));
+
+        // Apply the colors via style attributes
+        // This avoid dynamic CSS
+        $portletHeader.attr('style', 'background: ' + color);
+        $portlet.find('.homebox-portlet-inner').attr('style', 'border: 1px solid ' + color);
+        Drupal.homebox.pageChanged();
+      });
+
+      // Add tooltips to icons
+      //changed .tipsy to .tooltip and commented out gravity
+      $portlet.find('.portlet-icon').tooltip({
+        //gravity: 's',
+        title: function () {
+          switch ($(this).attr('class').replace('portlet-icon portlet-', '')) {
+          case 'close':
+            return Drupal.t('Close');
+          case 'maximize':
+            return Drupal.t('Maximize');
+          case 'minimize':
+            return Drupal.t('Minimize');
+          case 'minus':
+            return Drupal.t('Collapse');
+          case 'plus':
+            return Drupal.t('Expand');
+          case 'settings':
+            return Drupal.t('Settings');
+          }
+        }
+      });
+
+      // Remove tooltips on header clicks
+      $portletHeader.click(function () {
+        $('.tipsy').remove();
+      });
+    });
+  }
+};
+})(jQuery);
