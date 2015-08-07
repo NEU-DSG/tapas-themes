6bfb847c8eb42c4d88c0ad2acf436369bebc507f
Eli Zoller <zoller.eli@gmail.com>
2015-08-07 13:33:10 -0400

    template for toolbar blocks

--- tapas_redesign/template.php
+++ tapas_redesign/template.php
@@ -4,3 +4,9 @@
  * @file
  * template.php
  */
+
+ function tapas_redesign_preprocess_region(&$variables, $hook) {
+     if($variables['region'] == "toolbar"){
+         $variables['classes_array'][] = 'toolbar';
+     }
+ }
