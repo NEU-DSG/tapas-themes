4ba9ef9f7f7b2d949c8d5b1edbcb1abbf594df27
Eli Zoller <zoller.eli@gmail.com>
2015-08-25 08:03:49 -0400

    removing collapse for login/logout so the links work

--- tapas_redesign/template.php
+++ tapas_redesign/template.php
@@ -15,9 +15,9 @@
 function tapas_redesign_process(&$variables, $hook) {
   if(in_array("toolbar", $variables['classes_array'])){
     if(user_is_logged_in()){
-      $variables['content'] .= '<a class="collapsed toolbar-link" data-toggle="collapse" href="/user/logout" aria-expanded="false"><span class=" fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-sign-out fa-stack-1x fa-inverse"></i></span><span class="toolbar-title">Log out</span></a>';
+      $variables['content'] .= '<a class="toolbar-link" href="/user/logout" aria-expanded="false"><span class=" fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-sign-out fa-stack-1x fa-inverse"></i></span><span class="toolbar-title">Log out</span></a>';
     } else {
-      $variables['content'] = '<a class="collapsed toolbar-link" data-toggle="collapse" href="/user" aria-expanded="false"><span class=" fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-sign-in fa-stack-1x fa-inverse"></i></span><span class="toolbar-title">Sign In</span></a>' . $variables['content'];
+      $variables['content'] = '<a class="toolbar-link" href="/user" aria-expanded="false"><span class=" fa-stack fa-lg"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-sign-in fa-stack-1x fa-inverse"></i></span><span class="toolbar-title">Sign In</span></a>' . $variables['content'];
     }
   }
 }
