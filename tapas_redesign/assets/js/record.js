63ccc094ad176b1a29868c45e419a5135e4bfcfc
Eli Zoller <zoller.eli@gmail.com>
2015-10-21 14:24:10 -0400

    fixes for record js file

--- tapas_redesign/assets/js/record.js
+++ tapas_redesign/assets/js/record.js
@@ -17,20 +17,20 @@
       console.log(style_class);
       $('[class^="reader_"]').hide();
       $("link[id^='reader_css_']").each(function(){
-        $(this).attr('src','');
+        $(this).attr('href','');
       });
       $("#reader_js").attr('src','');
       $("."+style_class).show();
       if (style == 'teibp'){
         $("#reader_js").attr('src','http://rails_api.tapasdev.neu.edu/reading_interface/teibp/js/teibp.js');
-        $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/sleepy.css");
-        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/teibp.css");
+        $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/teibp.css");
+        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/sleepy.css");
         $("#reader_css_3").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/terminal.css");
       }
       if (style == 'tapas_generic'){
         $("#reader_js").attr('src','http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/js/tapas-generic.js');
-        $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGdiplo.css");
-        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGnormal.css");
+        $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGnormal.css");
+        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGdiplo.css");
       }
     }
   });
