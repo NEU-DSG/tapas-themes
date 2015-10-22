e2de486950bc3d8190f07a2e6870533991f7f15d
Eli Zoller <zoller.eli@gmail.com>
2015-10-22 13:54:58 -0400

    removing unnecessary functions

--- tapas_redesign/assets/js/record.js
+++ tapas_redesign/assets/js/record.js
@@ -24,12 +24,10 @@
         $("#reader_css_1").attr("href", "http://rails_api.localhost:8080/reading_interface/teibp/css/teibp.css");
         $("#reader_css_2").attr("href", "http://rails_api.localhost:8080/reading_interface/teibp/css/sleepy.css");
         $("#reader_css_3").attr("href", "http://rails_api.localhost:8080/reading_interface/teibp/css/terminal.css");
-        initialize_teibp();
       }
       if (style == 'tapas_generic'){
         $("#reader_css_1").attr("href", "http://localhost/~beekerz/tapas-generic/css/tapasGnormal.css");
         $("#reader_css_2").attr("href", "http://localhost/~beekerz/tapas-generic/css/tapasGdiplo.css");
-        initialize_tapas_generic();
       }
     }
   });
