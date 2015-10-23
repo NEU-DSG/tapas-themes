759b1f2832903ca44ce747a51e779b0fd951fe64
Eli Zoller <zoller.eli@gmail.com>
2015-10-22 21:33:16 -0400

    fixing more asset paths

--- tapas_redesign/assets/js/record.js
+++ tapas_redesign/assets/js/record.js
@@ -23,11 +23,11 @@
       if (style == 'teibp'){
         $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/teibp.css");
         $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/sleepy.css");
-        $("#reader_css_3").attr("href", "http://rails_api.localhost:8080/reading_interface/teibp/css/terminal.css");
+        $("#reader_css_3").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/teibp/css/terminal.css");
       }
       if (style == 'tapas_generic'){
         $("#reader_css_1").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGnormal.css");
-        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/tapas-generic/css/tapasGdiplo.css");
+        $("#reader_css_2").attr("href", "http://rails_api.tapasdev.neu.edu/reading_interface/tapas-generic/css/tapasGdiplo.css");
       }
     }
   });
