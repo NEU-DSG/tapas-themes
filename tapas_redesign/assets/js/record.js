12eacfc30f41a3f1f0d3cacb3206bd09398503ba
Eli Zoller <zoller.eli@gmail.com>
2016-06-03 13:55:00 -0400

    starting work on css and js for a two-pane reader, making teibp and record js behave within the reader pane that is passed to them instead of across a whole page and some minor css fixes

--- tapas_redesign/assets/js/record.js
+++ tapas_redesign/assets/js/record.js
@@ -1,33 +1,35 @@
 (function($) {
   $(document).ready(function() {
-    $(".reading").prepend("<link rel='stylesheet' type='text/css' id='reader_css_1'></link><link rel='stylesheet' type='text/css' id='reader_css_2'></link><link rel='stylesheet' type='text/css' id='reader_css_3'></link>");
-    var style = $(".form-item-reading-selector select[name='reading_selector']").val();
-    console.log(style);
-    show_style(style);
+    $(".reading").each(function(){
+      $(this).prepend("<link rel='stylesheet' type='text/css' id='reader_css_1'></link><link rel='stylesheet' type='text/css' id='reader_css_2'></link><link rel='stylesheet' type='text/css' id='reader_css_3'></link>");
+      var style = $(this).parents(".node").find(".form-item-reading-selector select[name='reading_selector']").val();
+      console.log(style);
+      show_style($(this), style);
+    });
 
     $(".form-item-reading-selector select[name='reading_selector']").on("change", function(e){
       e.preventDefault();
-      var style = $(".form-item-reading-selector select[name='reading_selector']").val()
+      var style = $(this).val();
       console.log(style);
-      show_style(style);
+      var pane = $(this).parents(".form-item").siblings(".reading");
+      show_style(pane, style);
     })
 
-    function show_style(style){
+    function show_style(pane, style){
       style_class = "reader_"+style;
-      console.log(style_class);
-      $('[class^="reader_"]').hide();
-      $("link[id^='reader_css_']").each(function(){
+      pane.find('[class^="reader_"]').hide();
+      pane.find("link[id^='reader_css_']").each(function(){
         $(this).attr('href','');
       });
-      $("."+style_class).show();
+      pane.find("."+style_class).show();
       if (style == 'teibp'){
-        $("#reader_css_1").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/teibp.css");
-        $("#reader_css_2").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/sleepy.css");
-        $("#reader_css_3").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/terminal.css");
+        pane.find("#reader_css_1").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/teibp.css");
+        pane.find("#reader_css_2").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/sleepy.css");
+        pane.find("#reader_css_3").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/teibp/css/terminal.css");
       }
       if (style == 'tapas_generic'){
-        $("#reader_css_1").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/tapas-generic/css/tapasGnormal.css");
-        $("#reader_css_2").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/tapas-generic/css/tapasGdiplo.css");
+        pane.find("#reader_css_1").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/tapas-generic/css/tapasGnormal.css");
+        pane.find("#reader_css_2").attr("href", "/profiles/buildtapas/themes/tapas-themes/tapas_redesign/lib/tapas-generic/css/tapasGdiplo.css");
       }
     }
   });
